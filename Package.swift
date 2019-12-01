4a1b38e15cff7519aae2f92047d53c3b739ff085
Mark DiFranco <mpdifranco@icloud.com>
2019-12-01 11:06:10 -0500

    Add platform to SPM

--- Package.swift
+++ Package.swift
@@ -1,9 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "SwinjectAutoregistration",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
     products: [
         .library(
             name: "SwinjectAutoregistration",
